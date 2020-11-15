f3dc7e36684c2ff170da8652c10649e09a90bf90
CraigSmith789 <craig789@gmail.com>
2020-11-15 18:33:10 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex) VALUES('Mr.Chocolate', 12, 'M');
+INSERT INTO bears (name, age, sex) VALUES('Rowdy', 15, 'M');
+INSERT INTO bears (name, age, sex) VALUES('Tabitha', 33, 'F');
+INSERT INTO bears (name, age, sex) VALUES('Sergeant Brown', 27, 'M');
+INSERT INTO bears (name, age, sex) VALUES('Melissa', 13, 'F');
+INSERT INTO bears (name, age, sex) VALUES('Grinch', 17, 'M');
+INSERT INTO bears (name, age, sex) VALUES('Wendy', 33, 'F');
+INSERT INTO bears (name, age, sex) VALUES(NULL, 67, 'M');
